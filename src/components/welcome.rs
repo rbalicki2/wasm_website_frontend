3d7d17e8781cd9c82f66de71a5385b2b84422898
Robert <robert.balicki@gmail.com>
2018-07-14 23:34:10 -0400

    add handlers

--- src/components/welcome.rs
+++ src/components/welcome.rs
@@ -11,6 +11,7 @@ impl Welcome {
 
 impl Component for Welcome {
   fn render(&self) -> HtmlToken {
-    jsx!(<div><div>this is <h1>welcome</h1></div></div>)
+    let click_handler: Box<FnOnce(Event) -> ()> = Box::new(|_| {});
+    jsx!(<div>hihihi<h1 OnClick={click_handler}>welcome</h1></div>)
   }
 }
