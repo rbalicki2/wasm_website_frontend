28314d61782c5abf813d8f45e2eb2a908852340b
Robert <robert.balicki@gmail.com>
2018-07-15 03:05:00 -0400

    add lifetime param

--- src/components/welcome.rs
+++ src/components/welcome.rs
@@ -1,7 +1,7 @@
 use jsx_macro::*;
 use jsx_types::*;
 
-use std::boxed::FnBox;
+// use std::boxed::FnBox;
 
 pub struct Welcome {
   click_count: i32,
@@ -15,9 +15,11 @@ impl Welcome {
   }
 }
 
-impl Component for Welcome {
-  fn render(&mut self) -> HtmlToken {
-    let click_handler: Box<FnBox(Event) -> ()> = Box::new(|_| {});
+impl<'a> Component<'a> for Welcome {
+  fn render(&'a mut self) -> HtmlToken<'a> {
+    let click_handler: Box<FnMut(Event) -> ()> = Box::new(|_| {
+      // self.click_count += 1;
+    });
     jsx!(<div><h1 OnClick={click_handler}>Ive been clicked {self.click_count} times</h1></div>)
   }
 }
