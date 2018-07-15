eb6f7eeb5e3293c98405403303fb0b790d45bf27
Robert <robert.balicki@gmail.com>
2018-07-15 02:01:56 -0400

    moar

--- src/components/welcome.rs
+++ src/components/welcome.rs
@@ -3,17 +3,21 @@ use jsx_types::*;
 
 use std::boxed::FnBox;
 
-pub struct Welcome {}
+pub struct Welcome {
+  click_count: i32,
+}
 
 impl Welcome {
   pub fn new() -> Welcome {
-    Welcome {}
+    Welcome {
+      click_count: 0,
+    }
   }
 }
 
 impl Component for Welcome {
   fn render(&mut self) -> HtmlToken {
     let click_handler: Box<FnBox(Event) -> ()> = Box::new(|_| {});
-    jsx!(<div><h1 OnClick={click_handler}>welcome</h1></div>)
+    jsx!(<div><h1 OnClick={click_handler}>Ive been clicked {self.click_count} times</h1></div>)
   }
 }
