c39c393d67596b94a689c0d5217ae9610ba89b2d
Robert Balicki <robert.balicki@gmail.com>
2018-07-21 01:18:20 -0400

    ayayayaya

--- src/components/welcome.rs
+++ src/components/welcome.rs
@@ -7,14 +7,12 @@ use std::rc::Rc;
 
 pub struct Welcome {
   click_count: i32,
-  is_hovering: bool,
 }
 
 impl Welcome {
   pub fn new() -> Welcome {
     Welcome {
       click_count: 0,
-      is_hovering: false,
     }
   }
 }
@@ -26,29 +24,21 @@ impl<'a> Component<'a> for Welcome {
 
     let cell = Rc::new(RefCell::new(self));
     let cell_2 = cell.clone();
-    let cell_3 = cell.clone();
-    let click_handler: Box<FnMut(Event) -> () + 'a> = Box::new(move |_| {
+    let increment: Box<FnMut(Event) -> () + 'a> = Box::new(move |_| {
       let mut s = cell.borrow_mut();
       s.click_count += 1;
     });
-    let on_mouse_over_handler: Box<FnMut(Event) -> () + 'a> = Box::new(move |_| {
+    let decrement: Box<FnMut(Event) -> () + 'a> = Box::new(move |_| {
       let mut s = cell_2.borrow_mut();
-      s.is_hovering = true;
+      s.click_count -= 1;
     });
-    let on_mouse_out_handler: Box<FnMut(Event) -> () + 'a> = Box::new(move |_| {
-      let mut s = cell_3.borrow_mut();
-      // s.is_hovering = false;
-      s.click_count = 0;
-    });
-    // let click_handler: () = click_handler;
-    // let click_handler_2: Box<FnMut(Event) -> ()> = Box::new(|_| {
-    //   self.click_count += 2;
-    // });
-    jsx!(<div
-      OnClick={click_handler}
-      OnMouseOut={on_mouse_out_handler}
-    >
+
+    let cursor_pointer = "cursor: pointer; user-select: none;";
+
+    jsx!(<div>
       I have been clicked {click_count}{times_pluralized}
+      <div OnClick={increment} style={cursor_pointer}>+</div>
+      <div OnClick={decrement} style={cursor_pointer}>-</div>
     </div>)
   }
 }
