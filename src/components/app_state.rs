b54d5bd3cd4b383a70260d7140e20f9ce3176ca2
Robert Balicki <robert.balicki@gmail.com>
2018-07-29 13:10:09 -0400

    todo item displays

--- src/components/app_state.rs
+++ src/components/app_state.rs
@@ -36,38 +36,46 @@ impl AppState {
     }
   }
 
-  // todo &mut self
-  pub fn create_todo_item(state: &mut AppState) {
+  pub fn create_todo_item(todo_items: &mut Vec<TodoItem>, current_text: &mut String) {
     let todo_item = TodoItem {
-      // TODO avoid cloning
-      text: state.current_text.clone(),
+      text: current_text.clone(),
       is_done: false,
     };
-    state.current_text = "".to_string();
-    state.todo_items.push(todo_item);
+    *current_text = "".to_string();
+    todo_items.push(todo_item);
   }
+}
+
+fn clone_many_times<T>(n: usize, cell: &T) -> Vec<T> where T: Clone {
+  let mut vec = Vec::with_capacity(n);
+  for _ in 0..n {
+    vec.push(cell.clone());
+  }
+  vec
 }
 
 impl<'a> Component<'a, ()> for AppState {
   fn render(&'a mut self, _props: ()) -> HtmlToken<'a> {
     let self_2 = self.clone();
-    let cell = Rc::new(RefCell::new(self));
-    let cell_2 = cell.clone();
-    let cell_3 = cell.clone();
-    let cell_4 = cell.clone();
+    let todo_items_cell = Rc::new(RefCell::new(&mut self.todo_items));
+    let todo_items_cells = clone_many_times(self_2.todo_items.len(), &todo_items_cell);
+    let current_text_cell = Rc::new(RefCell::new(&mut self.current_text));
+    let current_text_cell_2 = current_text_cell.clone();
+    let view_cell = Rc::new(RefCell::new(&mut self.view));
 
     let input_props: input::InputProps<'a> = input::InputProps {
       value: self_2.current_text,
       on_input: Box::new(move |e| {
-        let mut state = cell.borrow_mut();
+        let mut current_text = current_text_cell.borrow_mut();
         if let Some(ref val) = e.value {
-          state.current_text = val.to_string();
+          **current_text = val.to_string();
         }
       }),
       on_keydown: Box::new(move |e| {
-        let mut state = cell_2.borrow_mut();
+        let mut todo_items = todo_items_cell.borrow_mut();
+        let mut current_text = current_text_cell_2.borrow_mut();
         if e.key_code == 13 {
-          AppState::create_todo_item(&mut state);
+          AppState::create_todo_item(&mut todo_items, &mut current_text);
         }
       }),
     };
@@ -75,9 +83,9 @@ impl<'a> Component<'a, ()> for AppState {
     let view_picker_props = view_picker::ViewPickerProps {
       view: self_2.view,
       on_select_view: Box::new(
-        move |view| {
-          let mut state = cell_3.borrow_mut();
-          state.view = view;
+        move |new_view| {
+          let mut view = view_cell.borrow_mut();
+          **view = new_view;
         }
       ),
     };
@@ -91,29 +99,31 @@ impl<'a> Component<'a, ()> for AppState {
       }
     };
 
+    let todo_item_displays = self_2.todo_items
+      .iter()
+      .zip(todo_items_cells.into_iter())
+      .zip(0..self_2.todo_items.len())
+      .map(|((todo_item, rc), i)| (todo_item, rc, i))
+      .filter(|(t, _, _)| is_visible(t))
+      .map(|(todo_item, rc, i)| {
+        let todo_item = todo_item.clone();
+        let todo_item_display_props = todo_item_display::TodoItemDisplayProps {
+          todo_item,
+          on_complete_item: Box::new(move || {
+            let mut todo_items = rc.borrow_mut();
+            let todo_item = todo_items.get_mut(i).unwrap();
+            todo_item.is_done = !todo_item.is_done;
+          }),
+        };
+        todo_item_display::TodoItemDisplay::render(todo_item_display_props)
+      })
+      .collect::<Vec<HtmlToken>>();
+
     jsx!(<div>
       Smithy Todo List
       <div>{view_picker::ViewPicker::render(view_picker_props)}</div>
       <div>{input::Input::render(input_props)}</div>
-      <div>{
-        self_2.todo_items
-          .iter()
-          // .filter(is_visible) does not work. Why?
-          .filter(|t| is_visible(t))
-          .map(|todo_item| {
-            let cell = cell_4.clone();
-            let todo_item = todo_item.clone();
-            let todo_item_display_props = todo_item_display::TodoItemDisplayProps {
-              todo_item,
-              on_complete_item: Box::new(move || {
-                let mut state = cell.borrow_mut();
-                state.todo_items = vec![];
-              }),
-            };
-            todo_item_display::TodoItemDisplay::render(todo_item_display_props)
-          })
-          .collect::<Vec<HtmlToken>>()
-      }</div>
+      {todo_item_displays}
     </div>)
   }
 }
